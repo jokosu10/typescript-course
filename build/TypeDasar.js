cb85c4b3572c15d7a2bf65a1d8e07e322e0ac86f
Joko Susilo <susilo.j8@gmail.com>
2020-11-21 13:50:00 +0700

    type variable dasar string, number, boolean

---
+++ build/TypeDasar.js
@@ -0,0 +1,12 @@
+"use strict";
+// Type variable string
+var nickname = "jokosu10";
+nickname = "string";
+// Type variable number
+var age;
+age = 27;
+age = 17;
+// Type variable boolean
+var isMarried;
+isMarried = false;
+isMarried = true;
---
+++ src/TypeDasar.ts
@@ -0,0 +1,13 @@
+// Type variable string
+let nickname: string = "jokosu10";
+nickname = "string";
+
+// Type variable number
+let age: number;
+age = 27;
+age = 17;
+
+// Type variable boolean
+let isMarried: Boolean;
+isMarried = false;
+isMarried = true;
