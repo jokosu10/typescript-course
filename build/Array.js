1b8b76d6122cd1c3c14dae79da69929877ef3281
Joko Susilo <susilo.j8@gmail.com>
2020-11-21 20:46:43 +0700

    Learn type dasar array

---
+++ build/Array.js
@@ -0,0 +1,16 @@
+"use strict";
+// array number
+var array;
+array = [1, 2, 3];
+// array string
+var array2;
+array2 = ["string1", "string2"];
+// array any type data
+var array3;
+array3 = [1, "string", {}, true, "123456", "asiap"];
+// array tupples
+var biodata;
+biodata = ["Surabaya", 4];
+biodata = ["Malang", true];
+biodata = ["Sidoarjo", 3, false];
+biodata = [10, "Tegal"];
---
+++ src/Array.ts
@@ -0,0 +1,18 @@
+// array number
+let array: number[];
+array = [1, 2, 3];
+
+// array string
+let array2: string[];
+array2 = ["string1", "string2"];
+
+// array any type data
+let array3: any[];
+array3 = [1, "string", {}, true, "123456", "asiap"];
+
+// array tupples
+let biodata: [string, number];
+biodata = ["Surabaya", 4];
+biodata = ["Malang", true];
+biodata = ["Sidoarjo", 3, false];
+biodata = [10, "Tegal"];
